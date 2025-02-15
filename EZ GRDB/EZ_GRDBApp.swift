cec1be5489b2bb58f1e2e69e94300a0d12026725
ynom <nomatter@domain.com>
2025-02-15 11:28:30 -0800

    Initial Commit

---
+++ EZ GRDB/EZ_GRDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  EZ_GRDBApp.swift
+//  EZ GRDB
+//
+//  Created by ynom on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct EZ_GRDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EZ GRDBTests/EZ_GRDBTests.swift
@@ -0,0 +1,16 @@
+//
+//  EZ_GRDBTests.swift
+//  EZ GRDBTests
+//
+//  Created by ynom on 2/15/25.
+//
+
+import Testing
+
+struct EZ_GRDBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
